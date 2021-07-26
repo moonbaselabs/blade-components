d076dcf812dad55c9e9149d3d76c08466cba8e8d
Christian Taylor <christianbtaylor@gmail.com>
2021-07-26 16:21:11 -0500

    Add slot to select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,19 +1,27 @@
 @if(! $multiple)
   <select {{ $controlAttributes() }}>
-    @foreach($options as $v => $l)
-      @if($placeholder)
-        <option value hidden>{{ $placeholder }}</option>
-      @endif
-      <option {{ $isSelected($v) ? 'selected' : '' }} value="{{ $v }}">{{ $l }}</option>
-    @endforeach
+    @empty($options)
+      @foreach($options as $v => $l)
+        @if($placeholder)
+          <option value hidden>{{ $placeholder }}</option>
+        @endif
+        <option {{ $isSelected($v) ? 'selected' : '' }} value="{{ $v }}">{{ $l }}</option>
+      @endforeach
+    @else
+      {{ $slot }}
+    @endempty
   </select>
 @else
   <div id="{{ $id }}" {{ $controlAttributes() }}>
-    @foreach($options as $v => $l)
-      <x-label class="flex items-center space-x-2 w-full py-2 px-3 bg-white hover:bg-gray-50">
-        <x-checkbox name="{{ $name }}[]" value="{{ $v }}" :checked="$isSelected($v)" />
-        <span>{{ $l }}</span>
-      </x-label>
-    @endforeach
+    @empty($options)
+      @foreach($options as $v => $l)
+        <x-label class="flex items-center space-x-2 w-full py-2 px-3 bg-white hover:bg-gray-50">
+          <x-checkbox name="{{ $name }}[]" value="{{ $v }}" :checked="$isSelected($v)" />
+          <span>{{ $l }}</span>
+        </x-label>
+      @endforeach
+    @else
+      {{ $slot }}
+    @endempty
   </div>
 @endif
