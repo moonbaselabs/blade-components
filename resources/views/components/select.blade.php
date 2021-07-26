5d3252dc15754e6bba43f9920121191029895784
Christian Taylor <christianbtaylor@gmail.com>
2021-07-26 16:49:34 -0500

    Remove array suffix from multi select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -16,7 +16,7 @@
     @if(! empty($options))
       @foreach($options as $v => $l)
         <x-label for="{{ $name }}_{{ $loop->iteration }}" class="flex items-center space-x-2 w-full py-2 px-3 bg-white hover:bg-gray-50">
-          <x-checkbox name="{{ $name }}[]" id="{{ $name }}_{{ $loop->iteration }}" value="{{ $v }}" :checked="$isSelected($v)" />
+          <x-checkbox name="{{ $name }}" id="{{ $name }}_{{ $loop->iteration }}" value="{{ $v }}" :checked="$isSelected($v)" />
           <span>{{ $l }}</span>
         </x-label>
       @endforeach
